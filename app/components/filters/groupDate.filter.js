32102cefd3354558012e4f3c0e4c9d47d252b517
Silvio Rodrigues <silviorodrigues7@gmail.com>
2017-02-12 01:06:49 -0200

    Transfers list UI

--- app/components/filters/groupDate.filter.js
+++ app/components/filters/groupDate.filter.js
@@ -1,7 +1,9 @@
 angular.module('bluebank').filter('groupDate', function() {
   return function(input) {
     var date = input.split('/');
-    var output = date[1] + '/' + date[0];
+    day = date[1].length == 1 ? '0' + date[1] : date[1];
+    month = date[0].length == 1 ? '0' + date[0] : date[0];
+    var output = day + '/' + month;
     return output;
   }
 });
